26e97e41a0512345a626009635c90bf8a586765a
Michaell Alavedra Munayco <michael.munayco@eficientis.com>
2022-07-07 20:14:15 -0400

    test: threejs hello world

--- ngx-profile-avatar-app/projects/ngx-profile-avatar/src/lib/ngx-profile-avatar.component.ts
+++ ngx-profile-avatar-app/projects/ngx-profile-avatar/src/lib/ngx-profile-avatar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, AfterViewInit, Input, ViewChild, ElementRef } from '@angular/core';
-import * as THREE from "three";
+import * as THREE from 'three';
 import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
@@ -8,14 +8,14 @@ import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
 @Component({
 	selector: 'ngx-profile-avatar',
 	template: `
-    <canvas #canvas id="canvas" style="height: 100vh; width: 100vw;"></canvas>`,
+    <canvas #canvas id='canvas' style='height: 100vh; width: 100vw;'></canvas>`,
 	styles: []
 })
 export class NgxProfileAvatarComponent implements OnInit, AfterViewInit {
 
 	@ViewChild('canvas') private canvasRef: ElementRef;
 
-	//* Stage Properties
+	// Stage Properties
 
 	@Input() public source: string = '';
 
@@ -25,7 +25,8 @@ export class NgxProfileAvatarComponent implements OnInit, AfterViewInit {
 
 	@Input('farClipping') public farClippingPane: number = 1000;
 
-	//? Scene properties
+	// Scene properties
+
 	private camera: THREE.PerspectiveCamera;
 
 	private controls: OrbitControls;
@@ -44,7 +45,6 @@ export class NgxProfileAvatarComponent implements OnInit, AfterViewInit {
 
 	private directionalLight: THREE.DirectionalLight;
 
-	//? Helper Properties (Private Properties);
 
 	private get canvas(): HTMLCanvasElement {
 		return this.canvasRef.nativeElement;
@@ -56,24 +56,12 @@ export class NgxProfileAvatarComponent implements OnInit, AfterViewInit {
 
 	private scene: THREE.Scene;
 
-	/**
-	 *Animate the model
-	 *
-	 * @private
-	 * @memberof ModelComponent
-	 */
 	private animateModel() {
 		if (this.model) {
 			this.model.rotation.z += 0.005;
 		}
 	}
 
-	/**
-	 *create controls
-	 *
-	 * @private
-	 * @memberof ModelComponent
-	 */
 	private createControls = () => {
 		const renderer = new CSS2DRenderer();
 		renderer.setSize(window.innerWidth, window.innerHeight);
@@ -87,67 +75,59 @@ export class NgxProfileAvatarComponent implements OnInit, AfterViewInit {
 		this.controls.update();
 	};
 
-	/**
-	 * Create the scene
-	 *
-	 * @private
-	 * @memberof CubeComponent
-	 */
 	private createScene() {
-		//* Scene
+
+		// Scene
 		this.scene = new THREE.Scene();
 		this.scene.background = new THREE.Color(0xd4d4d8)
-		this.loaderGLTF.load(this.source, (gltf: GLTF) => {
+		this.loaderGLTF.load(this.source, gltf => {
 			this.model = gltf.scene.children[0];
 			console.log(this.model);
-			var box = new THREE.Box3().setFromObject(this.model);
-			box.getCenter(this.model.position); // this re-sets the mesh position
+			const box = new THREE.Box3().setFromObject(this.model);
+			box.getCenter(this.model.position);
 			this.model.position.multiplyScalar(-1);
 			this.scene.add(this.model);
 		});
-		//*Camera
-		let aspectRatio = this.getAspectRatio();
-		this.camera = new THREE.PerspectiveCamera(
-			this.fieldOfView,
-			aspectRatio,
-			this.nearClippingPane,
-			this.farClippingPane
-		)
+
+		// Camera
+		const aspectRatio = this.getAspectRatio();
+		this.camera = new THREE.PerspectiveCamera(this.fieldOfView, aspectRatio, this.nearClippingPane, this.farClippingPane);
 		this.camera.position.x = 100;
 		this.camera.position.y = 100;
 		this.camera.position.z = 100;
+
 		this.ambientLight = new THREE.AmbientLight(0x00000, 100);
 		this.scene.add(this.ambientLight);
+
 		this.directionalLight = new THREE.DirectionalLight(0xffdf04, 0.4);
 		this.directionalLight.position.set(0, 1, 0);
 		this.directionalLight.castShadow = true;
 		this.scene.add(this.directionalLight);
+
 		this.light1 = new THREE.PointLight(0x4b371c, 10);
 		this.light1.position.set(0, 200, 400);
 		this.scene.add(this.light1);
+
 		this.light2 = new THREE.PointLight(0x4b371c, 10);
 		this.light2.position.set(500, 100, 0);
 		this.scene.add(this.light2);
+
 		this.light3 = new THREE.PointLight(0x4b371c, 10);
 		this.light3.position.set(0, 100, -500);
 		this.scene.add(this.light3);
+
 		this.light4 = new THREE.PointLight(0x4b371c, 10);
 		this.light4.position.set(-500, 300, 500);
 		this.scene.add(this.light4);
+
 	}
 
 	private getAspectRatio() {
 		return this.canvas.clientWidth / this.canvas.clientHeight;
 	}
 
-	/**
- * Start the rendering loop
- *
- * @private
- * @memberof CubeComponent
- */
 	private startRenderingLoop() {
-		//* Renderer
+		// Renderer
 		// Use canvas element in template
 		this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas, antialias: true });
 		this.renderer.setPixelRatio(devicePixelRatio);
@@ -155,7 +135,7 @@ export class NgxProfileAvatarComponent implements OnInit, AfterViewInit {
 		let component = this;
 		(function render() {
 			component.renderer.render(component.scene, component.camera);
-			component.animateModel();
+			// component.animateModel();
 			requestAnimationFrame(render);
 		}());
 	}
