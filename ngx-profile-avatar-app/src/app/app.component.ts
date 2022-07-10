3c6f131ee8d42153bdfeac64f9b8d3785f6c5ed2
Michaell Alavedra <37460957+MichaellAlavedraMunayco@users.noreply.github.com>
2022-07-09 21:19:19 -0400

    Paola GLB

--- ngx-profile-avatar-app/src/app/app.component.ts
+++ ngx-profile-avatar-app/src/app/app.component.ts
@@ -8,7 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
 	url = 'https://d1a370nemizbjq.cloudfront.net/14ffadb0-f8ce-4d7c-aa0b-6d1dda85b694.glb';
-	tracker = 'cursor';
+	// url = 'https://d1a370nemizbjq.cloudfront.net/bdad8a36-075f-4e43-a8ae-45746a7f6a4e.glb';
+        tracker = 'cursor';
 
 	onLoadingAvatar({ loaded, total }: ProgressEvent) {
 		console.log(`Loading : ${Math.round(loaded / total * 100)}%`);
