9eb644d53f4b1d6875ea8db25b66b2107c96171d
Michaell Alavedra Munayco <michael.munayco@eficientis.com>
2022-08-27 17:09:19 -0400

    feat: demo app

--- ngx-profile-avatar-app/src/environments/environment.prod.ts
+++ ngx-profile-avatar-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  menURL: 'https://github.com/gitchaell/ngx-profile-avatar/blob/main/.github/images/men.avatar.glb?raw=true',
-  womenURL: 'https://github.com/gitchaell/ngx-profile-avatar/blob/main/.github/images/women.avatar.glb?raw=true',
+  menURL: 'https://raw.githubusercontent.com/gitchaell/ngx-profile-avatar/main/.github/images/men.avatar.glb',
+  womenURL: 'https://raw.githubusercontent.com/gitchaell/ngx-profile-avatar/main/.github/images/women.avatar.glb',
 };
